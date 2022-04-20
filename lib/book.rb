215a2e8be6c30718264d6715cc54fd956f47f39c
taylorvercillo <taylornvcasey@gmail.com>
2022-04-19 21:16:53 -0600

    commit tree ac10c8635f254338c42833fb453e6cff81e6226b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+require "pry"
+
+class Book 
+    attr_reader :title, :author, :page_count, :genre
+
+    def initialize(title, author, page_count, genre)
+        @title = title 
+        @author = author
+        @page_count = page_count 
+        @genre = genre
+    end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+
+class Shoe 
+
+    attr_accessor :brand
+
+    def initialize(brand)
+        @brand = brand
+    end 
+
+    def color 
+        @color 
+    end 
+
+    def color= (color)
+        @color = color
+    end
+
+end
